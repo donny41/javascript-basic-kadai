23f7dcc701c7e8f1b3b5777ea18bb9518fd2cd9b
donny41 <dyuandyu@gmail.com>
2024-06-07 00:46:20 +0900

    kadai_020提出

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+const text01 = document.getElementById('text');
+
+btn.addEventListener('click', () =>{
+    text01.textContent = "ボタンをクリックしました";    
+})
